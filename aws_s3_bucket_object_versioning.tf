1a02271767c50a92e40127b8b586732944249f02
papiamento <1024836+papiamento@users.noreply.github.com>
2020-03-18 15:52:21 -0400

    testing versioning

---
+++ aws_s3_bucket_object_versioning.tf
@@ -0,0 +1,13 @@
+
+resource "aws_s3_bucket_object" "s3-standard" {
+  bucket = aws_s3_bucket.terraform-templates-aws-s3-versioning.bucket
+  key    = "versioning.txt"
+  source = "versioning.txt"
+
+  # most expensive storage tier
+  storage_class = "INTELLIGENT_TIERING"
+
+  etag = filemd5("versioning.txt")
+
+  acl = "public-read"
+}
