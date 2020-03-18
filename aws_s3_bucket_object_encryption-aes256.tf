ba7474e1280e61e65731f434cfaaf3e5d4aed416
papiamento <1024836+papiamento@users.noreply.github.com>
2020-03-18 15:30:09 -0400

    set server side encryption

--- aws_s3_bucket_object_encryption-aes256.tf
+++ aws_s3_bucket_object_encryption-aes256.tf
@@ -1,8 +1,10 @@
-
 resource "aws_s3_bucket_object" "s3-encryption-aes256" {
   bucket = aws_s3_bucket.terraform-templates-aws-s3.bucket
-  key    = "encryption-aes256.txt"
+  key = "encryption-aes256.txt"
   source = "encryption-aes256.txt"
+
+  # aws managed key
+  server_side_encryption = "AES256"
 
   # most expensive storage tier
   storage_class = "INTELLIGENT_TIERING"
