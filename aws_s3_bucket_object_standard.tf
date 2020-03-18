50815454933241a8b548b427d7afc5bb0a96da99
papiamento <1024836+papiamento@users.noreply.github.com>
2020-03-18 10:13:53 -0400

    storage class standard example

--- aws_s3_bucket_object_standard.tf
+++ aws_s3_bucket_object_standard.tf
@@ -2,7 +2,7 @@
 resource "aws_s3_bucket_object" "s3-standard" {
   bucket = aws_s3_bucket.terraform-templates-aws-s3.bucket
   key    = "s3-standard"
-  source = "sample.txt"
+  source = "storage_class_standard.txt"
 
   # most expensive storage tier
   storage_class = "STANDARD"
